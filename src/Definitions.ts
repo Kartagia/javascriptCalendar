90a27ae40c4ef0c948645d25a3f5b858a6619abf
Antti Kautiainen <antti@kautiainen.com>
2024-01-28 21:49:10 +0200

    refactor: Definitions
    new file:   src/Definitions.ts
    - namespace Definitions moved to itsn own file:
    - enum BaseFields - the base fields of a calendar.
    - enum DerivedFields - the fields whose valeu is derived from
      some set of derived fields.
    - type GenericField - the type of a generic fields.
    - function isGenericField - the gate keeper of passing generic fields.
    
    new file:   src/temporal.ts
    - A file gathering Temporal interfaces.
    - interface FieldDefinition - for defining a field.
    - interface TemporalVFiedlyalue - to represent single field value.
    - interface TemporalFieldsInstance - an instance combining several
      temporal feild valeu, and returning a record from field value to
        the field value.
    - utility function createRecord
     - Create a Record from fields to field values.
    
    modified:   src/VagueValueRange.ts
    - Adding ordered to the integer value range.
    - Gatekeeper function isOrdered to detemrine if a person is ordered.
    - fix: Changing the undefined boundary value to more accureate desing.

---
+++ src/Definitions.ts
@@ -0,0 +1,235 @@
+import { Integer, getComparisonResult } from "./comparison";
+
+/**
+ * The definitions of the calendar and temporal fields.
+ */
+export namespace Definitions {
+  /**
+   * The temporal interval names.
+   */
+  export enum Intervals {
+    DAYS = "days",
+    WEEKS = "weeks",
+    MONTHS = "months",
+    QUARTERS = "quarters",
+    YEARS = "years",
+    DECENNIALS = "decennials",// The ten years.
+    CENTURYS = "centuries",// The century. 
+    CYCLES = "cycles",// The cycle of the calendar, if it has any.
+    ERAS = "eras"
+  }
+
+  /**
+   * The base intervals.
+   */
+  export type BaseInterval = (Intervals.DAYS | Intervals.MONTHS | Intervals.YEARS | Intervals.ERAS);
+
+  /**
+   * The derived intervals.
+   */
+  export type DerivedInterval = Exclude<Interval, BaseInterval>;
+
+  /**
+   * The type of the intervals.
+   */
+  export type Interval = keyof typeof Intervals;
+
+  /**
+   * The type of the base fields.
+   *
+   * The only mandatory fields are DAY_OF_YEAR and CANONICAL_YEAR.
+   * The YEAR may be replaced with either YEAR or both YEAR_OF_ERA and ERA.
+   */
+  export enum BaseFields {
+    /**
+     * The generic day field.
+     */
+    day = "day",
+    /**
+     * The day of year is combined with YEAR.
+     */
+    dayOfYear = "dayOfYear",
+    /**
+     * The generic year. Defaults to CANONICAL_YEAR.
+     */
+    year = "year",
+    /**
+     * A canonical year represents a continuous year increasing towards
+     * the future.
+     */
+    canonicalYear = "canonicalYear"
+  }
+
+  /**
+   * The optional fields derived from either the CANONICAL_YEAR
+   * or from DAY_OF_YEAR and CANONICAL_YEAR.
+   */
+  export enum DerivedFields {
+
+    /**
+     * The date consitss of DAY, MONTH, YEAR, and possibly ERA.
+     */
+    date = "date",
+
+    /**
+     * A week represents either a week. The week scope is DAYS.
+     */
+    week = "week",
+
+    /**
+     * The day of week.
+     */
+    dayOfWeek = "dayOfWeek",
+
+    /**
+     * The day of month is always combined with a month.
+     */
+    dayOfMonth = "dayOfMonth",
+
+    /**
+     * The week of month.
+     */
+    weekOfMonth = "weekOfMonth",
+
+    /**
+     * The week of year.
+     */
+    weekOfYear = "weekOfYear",
+
+
+    /**
+     * The month is a month of year by default, but other calendars may
+     * implement a month of quarter.
+     */
+    month = "month",
+
+    /**
+     * The month of quarter.
+     */
+    monthOfQuarter = "monthOfQuarter",
+
+    /**
+     * A month of year is tied to a YEAR.
+     */
+    monthOfYear = "monthOfYear",
+
+    /**
+     * A quarter represents a quarter.
+     */
+    quarter = "quarter",
+
+    /**
+     * The quarter of year.
+     */
+    quarterOfYear = "quarterOfYear",
+    /**
+     * A year of era. The direction of increment of the year of era is not
+     * limited.
+     */
+    yearOfEra = "yearOfEra",
+    /**
+     * The era represents the era.
+     */
+    era = "era"
+  }
+
+  /**S
+   * The generic fields.
+   */
+  export type GenericField = (BaseFields.day | DerivedFields.week | DerivedFields.month | DerivedFields.quarter | BaseFields.year |
+    DerivedFields.era);
+
+  /**
+   * Is the feild a generic field.
+   * @param field The tested field.
+   * @returns True, if and only if the field is a generic field.
+   */
+  export function isGenericField(field: string): field is GenericField {
+    return [BaseFields.day,DerivedFields.week, DerivedFields.month, DerivedFields.quarter, BaseFields.year, 
+      DerivedFields.era].find( (value) => (value === field)) != null;
+  }
+  
+
+  /**
+   * The type of a base feild.
+   */
+  export type BaseField = keyof typeof BaseFields;
+
+  /**
+   * The type of a derived feild.
+   */
+  export type DerivedField = keyof typeof DerivedFields;
+
+  /**
+   * A field known to the defintions.
+   */
+  export type Field = (BaseField | DerivedField);
+
+  /**
+   * A temporal instance is a generic field containing instance.
+   */
+  export type TemporalInstance = Partial<{
+    [Property in GenericField]: Integer;
+  }>;
+
+
+  /**
+   * Get the base field of given temporal instance.
+   * @param fields The temporal fields instance.
+   * @returns The base field matching to the combination
+   * of the fields in the temporal instance.
+   */
+  export function getField(fields: TemporalInstance, baseField: GenericField): Field | undefined {
+
+    if (!(baseField in fields)) {
+      // The temporal instance does not have the generic field.
+      return undefined;
+    }
+
+    switch (baseField) {
+      case BaseFields.day:
+        if (DerivedFields.week in fields) {
+          return DerivedFields[DerivedFields.dayOfWeek];
+        } else if (DerivedFields.month in fields) {
+          return DerivedFields[DerivedFields.dayOfMonth];
+        } else if (BaseFields.year in fields) {
+          return BaseFields[BaseFields.dayOfYear];
+        } else {
+          return undefined;
+        }
+      case BaseFields.year:
+        if (DerivedFields.era in fields) {
+          return DerivedFields[DerivedFields.yearOfEra];
+        } else {
+          return BaseFields[BaseFields.canonicalYear];
+        }
+      case DerivedFields.week:
+        if (DerivedFields.month in fields) {
+          return DerivedFields[DerivedFields.weekOfMonth];
+        } else if (BaseFields.year in fields) {
+          return DerivedFields[DerivedFields.weekOfYear];
+        } else {
+          return undefined;
+        }
+      case DerivedFields.month:
+        if (DerivedFields.quarter in fields) {
+          return DerivedFields[DerivedFields.monthOfQuarter];
+        } else if (BaseFields.year in fields) {
+          return DerivedFields[DerivedFields.monthOfYear];
+        } else {
+          return undefined;
+        }
+      case DerivedFields.quarter:
+        if (BaseFields.year in fields) {
+          return DerivedFields[DerivedFields.quarterOfYear];
+        } else {
+          return undefined;
+        }
+      case DerivedFields.era:
+        return DerivedFields[baseField];
+      default:
+        return undefined;
+    }
+  } // funciton getField
+} // namespace Definitions
+
---
+++ src/temporal.ts
@@ -0,0 +1,306 @@
+
+import {ComparableInteger, Integer, PositiveInteger} from "./comparison";
+import { ValueRange } from "./ValueRange";
+import { VagueValueRange } from "./VagueValueRange";
+import { Definitions } from "./Definitions";
+
+/**
+ * @module temporal
+ * The module defining the temporal data types. 
+ */
+
+/**
+ * The velue type of the temporal values.
+ * (The canonical year requires integer instead of positive integer)
+ */
+export type ValueType = (Integer|PositiveInteger|ComparableInteger);
+
+
+/**
+ * A temporal instance placeholder.
+ */
+export type TemporalInstance = {[fieldName: string]:Integer};
+
+/**
+ * The function determining the field definition from the field name and the value.
+ * @param fieldName The field name of the defined field.
+ * @param value The current value used to determine the refinement of the definition.
+ * @returns The field definition of the given field with the given value.
+ * @throws UnsupportedFieldException The field is not supported.
+ * @throws RangeError The given value was invalid. 
+ */
+export type FieldDefinitionFunction<Type = number> = (fieldName: string, value?:Type) => FieldDefinition;
+
+
+/**
+ * The definition of a field.
+ */
+export interface FieldDefinition {
+  /**
+   * The field name.
+   */
+  readonly fieldName: string;
+  /**
+   * The field as object, if it exists.
+   */
+  readonly field: TemporalFieldly | undefined;
+
+  /**
+   * The range of valid values.
+   */
+  readonly range: (ValueRange<ComparableInteger>|VagueValueRange<ComparableInteger>);
+
+  /**
+   * The supported fields of the defined fields. 
+   */
+  readonly supportedFields?: Record<string, FieldDefinition|FieldDefinitionFunction<number|ComparableInteger>>;
+}
+
+
+/**
+ * Temporal value range.
+ */
+export type TemporalValueRange = ValueRange<ComparableInteger>|VagueValueRange<ComparableInteger>
+
+/**
+ * The interface of the temporal fieldly for storing a temporal fielld.
+ */
+export interface TemporalFieldly {
+  /**
+   * The base accuracy of the temporal field.
+   */
+  readonly baseField: Readonly<TemporalFieldly> | undefined;
+
+  /**
+   * The name of the field.
+   */
+  readonly fieldName: string;
+
+  /**
+   * The supported fields. This list must include the field name.
+   */
+  readonly supportedFieldsNames: Readonly<string[]>;
+	
+  /**
+   * The supported field definitions. This list must include the field name
+   * for definition of the valid values.
+   */
+  readonly supportedFieldDefinitions: Readonly<Record<string, FieldDefinition|FieldDefinitionFunction<Integer>>>;
+    
+  /**
+   * The fields equivalent with the temporal fieldly.
+   */
+  readonly equivalentFields: Readonly<TemporalFieldSpec[]>;
+    
+  /**
+   * Create an instance of the field.
+   * @param value The value of the field.
+   * @throws {RangeError} The field value was invalid.
+   */
+  createInstance(value: number): Readonly<TemporalFieldlyValue>;
+    
+  /**
+   * Get value range of the temporal field.
+   * @param field The temporal field.
+   * @throws {UnsupportedFieldException} The field is not supported by the temporal fieldly.
+   */
+  getValueRange(field: TemporalFieldly): Readonly<TemporalValueRange>;
+
+  /**
+   * Get value range of the current temporal field with value.
+   * @param value The value of the current field.
+   * @throws {RangeError} The given field value is invalid.
+   */
+  getValueRange(value: number | ComparableInteger): Readonly<TemporalValueRange>;
+
+  /**
+   * Get value range of the current temporal field with value.
+   * @return The value range of the current field.
+   */
+  getValueRange(): TemporalValueRange;
+
+    /**
+     * Get value range of the temporal field with value.
+     * @param field The temporal field.
+     * @param value The value of the current field.
+     * @throws {UnsupportedFieldException} The field is not supported by the temporal fieldly.
+     * @throws {RangeError} The given field value is invalid.
+     */
+    getValueRange(
+      field: TemporalFieldly,
+      value: number | ComparableInteger
+    ): Readonly<TemporalValueRange>;
+  
+  
+}
+
+/**
+ * The type of the temporal field sepcification..
+ */
+export type TemporalFieldSpec = (string|TemporalFieldly);
+
+
+/**
+ * The temporal fieldly value. All temporal fieldlys store values
+ * as numbers.
+ */
+export interface TemporalFieldlyValue {
+  /**
+   * The field whose value the temporal fieldly represents.
+   */
+  get field(): TemporalFieldly;
+
+  /**
+   * The temporal field value. If the value is undefined, teh field has no value.
+   */
+  get value(): Integer | undefined;
+
+  /**
+   * The getter of the field name.
+   */
+  get fieldName(): string;
+
+  /**
+   * Derive another field instance of hte same temporal field with given value.
+   * @param value The new value of the derived field.
+   * @throws {InvalidFieldValue} The given field value was invalid.
+   */
+  derive(value: Integer|ComparableInteger): TemporalFieldlyValue;
+
+  /**
+   * Create a new instance of a derived field with given value.
+   * @param field The field of the derived value.
+   * @param fieldValue The value of the derived. field.
+   * @throws {UnsupportedFieldException} The given field is not supported.
+   * @throws {InvalidFieldValue} The given field value was invalid.
+   */
+  derive(field: Definitions.Field|string, fieldValue: Integer|ComparableInteger): TemporalFieldlyValue;
+
+  /**
+   * Get a derived field.
+   * @param field The field, whose value is used to refine the value.
+   * @param value The value of the field.
+   * @throws {UnsupportedFieldException} The derived field is not supported by the
+   * current field.
+   */
+  deriveField(
+    field: TemporalFieldly,
+    value: Integer | ComparableInteger
+  ): TemporalFieldly;
+
+  /**
+   * Get the valeu range of the field.
+   * @param field The temporal field.
+   * @throws {UnsupportedFieldException} The given field is not supported by the current field value.
+   */
+  getFieldRange(field: TemporalFieldly): TemporalValueRange;
+
+  /**
+   * Converts the velue into number.
+   * @returns The primitive representation of the temporal fiedly value is either a number.
+   */
+  valueOf(): Integer;
+}
+
+/**
+ * An instance of one or more temporal feilds and their values.
+ */
+export interface TemporalFieldsInstance {
+
+  /**
+   * The base fields required for the temporal instance.
+   */
+  getBaseFields(): Readonly<Set<string|Definitions.Field|TemporalFieldly>>;
+
+  /**
+   * The set of the field values derived from the base values or with fixed value.
+   */
+  getDerivedFields(): Readonly<Set<string|Definitions.Field|TemporalFieldly>>;
+
+  /**
+   * Get the field value.
+   * @returns The field value, if the field belongs to the fields, or an undefined value.
+   */
+  get(field: Readonly<string|Definitions.Field|TemporalFieldly>): Integer|undefined;
+
+  getFieldvalues
+
+  /**
+   * Does the temporal instance support the field.
+   * @param field The tested field.
+   * @reutrns True, if and only if the temporal fieldly has the given field.
+   */
+  has(field: Readonly<string|Definitions.Field|TemporalFieldly>): boolean;
+
+  /**
+   * Create a new temporal fields instance from the current one with given field set to given value.
+   * @param newValue The new value of the field.
+   * @throws {InvalidFieldValue} The given field value is not valid.
+   * @throws {UnsupportedFieldException} The given field is not supported.
+   */
+  derive(field: Readonly<string|Definitions.Field|TemporalFieldly>, newValue: Integer): TemporalFieldsInstance;
+
+  /**
+   * The record representation of the temporal field instance.
+   * @returns The record with field names as keys and the field values as values. 
+   */
+  toRecord(): Record<string, Integer>;
+}
+
+/**
+ * Createa a record from feild value mapping or record.
+ * @param values The values of all fields.
+ * @param required The required fields. 
+ * @param optional The optional fields.
+ * @param defaultValues The mapping from field name to the default vaolue of the field, or to 
+ * a function determining the value of the field from the set of the required field values int eh order
+ * of the required fields set.
+ * @returns 
+ */
+export function createRecord(values: Map<string, Integer>|Record<string, Integer>, 
+    required: Set<string|Definitions.Field|TemporalFieldly>, 
+    optional?: Set<string|Definitions.Field|TemporalFieldly>,
+    defaultValues?: Map<string, Integer|((values: Set<Integer>)=>Integer)>
+    ): Record<string,Integer>  {
+
+  const result = {};
+  const getValue : (field: string) => Integer|undefined = (values instanceof Map ? (field: string) => (values.get(field)) : 
+  (field: string) => (field in values ? values[field] : undefined))
+  const getField : (field: string|Definitions.Field|TemporalFieldly) => string = (field) => {
+    return (typeof field === "object")? field.fieldName : field;
+  };
+  const getDefaultValue : ( value : Integer|((values: Set<Integer>)=>Integer), requiredFields: Set<Integer>) => (Integer|undefined) = 
+    (value, requiredFields) => {
+      if (typeof value === "function") {  
+        return value(requiredFields);
+      } else {
+        return value;
+      }
+    };
+  [...required.values()].forEach(element => {
+    const fieldName = getField(element);
+    const value = getValue(fieldName);
+    if (value === undefined) {
+      throw new RangeError(`Missing required field ${fieldName}`);
+    } else {
+      result[fieldName] = value;
+    }
+  });
+  [...(optional?.values()||[])].forEach(element => {
+    const fieldName = getField(element);
+    const value = getValue(fieldName);
+    if (value !== undefined) {
+      result[fieldName] = value;
+    } else if (defaultValues?.has(fieldName)) {
+      result[fieldName] = getDefaultValue(defaultValues.get(fieldName), new Set([...required.values()].map( (reqField) => {
+        const fieldName = getField(reqField);
+        return getValue(fieldName)
+      if (result[fieldName] === "undefined") {
+        delete result[fieldName];
+      }
+    })))
+    }
+  });
+
+  return result;
+}

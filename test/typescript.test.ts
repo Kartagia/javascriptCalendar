d5f790987021c9da4e314e0bd2d8302c81ca7d55
Antti Kautiainen <antti@kautiainen.com>
2024-01-30 14:30:33 +0200

    test: Testing the test environment for typescript
    new file:   test/typescript.test.ts
    - Simple test testing that the test environment works.

---
+++ test/typescript.test.ts
@@ -0,0 +1,9 @@
+
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+describe("Typescript testing works", () => {
+  it("Testing works", () => {
+    expect(true).to.be.true;
+  })
+})
